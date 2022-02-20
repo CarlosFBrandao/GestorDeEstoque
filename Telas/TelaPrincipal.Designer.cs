57ee2c870eaeb4e1a9b9f557f90be2f19ce5996d
Carlos Machado <carlos.machado.aluno@faculdadepromove.br>
2022-02-20 11:36:59 -0300

    Adicionar arquivos de projeto.

---
+++ Telas/TelaPrincipal.Designer.cs
@@ -0,0 +1,390 @@
+﻿
+namespace GestorDeEstoque.Telas
+{
+    partial class TelaPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.cadastrosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.unidadeDeMedidaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.produtosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblSigla = new System.Windows.Forms.Label();
+            this.lblDescricao = new System.Windows.Forms.Label();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.txtSigla = new System.Windows.Forms.TextBox();
+            this.txtDescricao = new System.Windows.Forms.TextBox();
+            this.PainelCadastroUnidadeMedida = new System.Windows.Forms.Panel();
+            this.panelCadastroProduto = new System.Windows.Forms.Panel();
+            this.txtQuantidade = new System.Windows.Forms.NumericUpDown();
+            this.comboUnd = new System.Windows.Forms.ComboBox();
+            this.txtvalor = new System.Windows.Forms.NumericUpDown();
+            this.txtDescricaoProduto = new System.Windows.Forms.TextBox();
+            this.txtCodigoProduto = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.PainelBotoes = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.btnVoltar = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.PainelCadastroUnidadeMedida.SuspendLayout();
+            this.panelCadastroProduto.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.txtQuantidade)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtvalor)).BeginInit();
+            this.PainelBotoes.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cadastrosToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // cadastrosToolStripMenuItem
+            // 
+            this.cadastrosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.unidadeDeMedidaToolStripMenuItem,
+            this.produtosToolStripMenuItem});
+            this.cadastrosToolStripMenuItem.Name = "cadastrosToolStripMenuItem";
+            this.cadastrosToolStripMenuItem.Size = new System.Drawing.Size(71, 20);
+            this.cadastrosToolStripMenuItem.Text = "Cadastros";
+            // 
+            // unidadeDeMedidaToolStripMenuItem
+            // 
+            this.unidadeDeMedidaToolStripMenuItem.Name = "unidadeDeMedidaToolStripMenuItem";
+            this.unidadeDeMedidaToolStripMenuItem.Size = new System.Drawing.Size(177, 22);
+            this.unidadeDeMedidaToolStripMenuItem.Text = "Unidade de Medida";
+            this.unidadeDeMedidaToolStripMenuItem.Click += new System.EventHandler(this.unidadeDeMedidaToolStripMenuItem_Click);
+            // 
+            // produtosToolStripMenuItem
+            // 
+            this.produtosToolStripMenuItem.Name = "produtosToolStripMenuItem";
+            this.produtosToolStripMenuItem.Size = new System.Drawing.Size(177, 22);
+            this.produtosToolStripMenuItem.Text = "Produtos";
+            this.produtosToolStripMenuItem.Click += new System.EventHandler(this.produtosToolStripMenuItem_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 65);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(757, 327);
+            this.dataGridView1.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Código:";
+            // 
+            // lblSigla
+            // 
+            this.lblSigla.AutoSize = true;
+            this.lblSigla.Location = new System.Drawing.Point(115, 17);
+            this.lblSigla.Name = "lblSigla";
+            this.lblSigla.Size = new System.Drawing.Size(33, 13);
+            this.lblSigla.TabIndex = 3;
+            this.lblSigla.Text = "Sigla:";
+            // 
+            // lblDescricao
+            // 
+            this.lblDescricao.AutoSize = true;
+            this.lblDescricao.Location = new System.Drawing.Point(221, 17);
+            this.lblDescricao.Name = "lblDescricao";
+            this.lblDescricao.Size = new System.Drawing.Size(58, 13);
+            this.lblDescricao.TabIndex = 4;
+            this.lblDescricao.Text = "Descrição:";
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(12, 33);
+            this.txtCodigo.MaxLength = 10;
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(100, 20);
+            this.txtCodigo.TabIndex = 5;
+            // 
+            // txtSigla
+            // 
+            this.txtSigla.Location = new System.Drawing.Point(118, 33);
+            this.txtSigla.MaxLength = 3;
+            this.txtSigla.Name = "txtSigla";
+            this.txtSigla.Size = new System.Drawing.Size(100, 20);
+            this.txtSigla.TabIndex = 6;
+            // 
+            // txtDescricao
+            // 
+            this.txtDescricao.Location = new System.Drawing.Point(224, 33);
+            this.txtDescricao.MaxLength = 200;
+            this.txtDescricao.Name = "txtDescricao";
+            this.txtDescricao.Size = new System.Drawing.Size(545, 20);
+            this.txtDescricao.TabIndex = 7;
+            // 
+            // PainelCadastroUnidadeMedida
+            // 
+            this.PainelCadastroUnidadeMedida.Controls.Add(this.txtDescricao);
+            this.PainelCadastroUnidadeMedida.Controls.Add(this.txtSigla);
+            this.PainelCadastroUnidadeMedida.Controls.Add(this.txtCodigo);
+            this.PainelCadastroUnidadeMedida.Controls.Add(this.lblDescricao);
+            this.PainelCadastroUnidadeMedida.Controls.Add(this.lblSigla);
+            this.PainelCadastroUnidadeMedida.Controls.Add(this.label1);
+            this.PainelCadastroUnidadeMedida.Controls.Add(this.dataGridView1);
+            this.PainelCadastroUnidadeMedida.Location = new System.Drawing.Point(12, 43);
+            this.PainelCadastroUnidadeMedida.Name = "PainelCadastroUnidadeMedida";
+            this.PainelCadastroUnidadeMedida.Size = new System.Drawing.Size(776, 395);
+            this.PainelCadastroUnidadeMedida.TabIndex = 2;
+            this.PainelCadastroUnidadeMedida.Visible = false;
+            // 
+            // panelCadastroProduto
+            // 
+            this.panelCadastroProduto.Controls.Add(this.dataGridView2);
+            this.panelCadastroProduto.Controls.Add(this.txtQuantidade);
+            this.panelCadastroProduto.Controls.Add(this.comboUnd);
+            this.panelCadastroProduto.Controls.Add(this.txtvalor);
+            this.panelCadastroProduto.Controls.Add(this.txtDescricaoProduto);
+            this.panelCadastroProduto.Controls.Add(this.txtCodigoProduto);
+            this.panelCadastroProduto.Controls.Add(this.label6);
+            this.panelCadastroProduto.Controls.Add(this.label5);
+            this.panelCadastroProduto.Controls.Add(this.label4);
+            this.panelCadastroProduto.Controls.Add(this.label3);
+            this.panelCadastroProduto.Controls.Add(this.label2);
+            this.panelCadastroProduto.Location = new System.Drawing.Point(12, 43);
+            this.panelCadastroProduto.Name = "panelCadastroProduto";
+            this.panelCadastroProduto.Size = new System.Drawing.Size(776, 395);
+            this.panelCadastroProduto.TabIndex = 3;
+            this.panelCadastroProduto.Visible = false;
+            // 
+            // txtQuantidade
+            // 
+            this.txtQuantidade.DecimalPlaces = 5;
+            this.txtQuantidade.Location = new System.Drawing.Point(250, 78);
+            this.txtQuantidade.Name = "txtQuantidade";
+            this.txtQuantidade.Size = new System.Drawing.Size(100, 20);
+            this.txtQuantidade.TabIndex = 9;
+            // 
+            // comboUnd
+            // 
+            this.comboUnd.FormattingEnabled = true;
+            this.comboUnd.Location = new System.Drawing.Point(118, 77);
+            this.comboUnd.Name = "comboUnd";
+            this.comboUnd.Size = new System.Drawing.Size(121, 21);
+            this.comboUnd.TabIndex = 8;
+            // 
+            // txtvalor
+            // 
+            this.txtvalor.DecimalPlaces = 5;
+            this.txtvalor.Location = new System.Drawing.Point(12, 78);
+            this.txtvalor.Name = "txtvalor";
+            this.txtvalor.Size = new System.Drawing.Size(100, 20);
+            this.txtvalor.TabIndex = 7;
+            // 
+            // txtDescricaoProduto
+            // 
+            this.txtDescricaoProduto.Location = new System.Drawing.Point(118, 33);
+            this.txtDescricaoProduto.Name = "txtDescricaoProduto";
+            this.txtDescricaoProduto.Size = new System.Drawing.Size(644, 20);
+            this.txtDescricaoProduto.TabIndex = 6;
+            // 
+            // txtCodigoProduto
+            // 
+            this.txtCodigoProduto.Location = new System.Drawing.Point(12, 33);
+            this.txtCodigoProduto.Name = "txtCodigoProduto";
+            this.txtCodigoProduto.Size = new System.Drawing.Size(100, 20);
+            this.txtCodigoProduto.TabIndex = 5;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(9, 62);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(34, 13);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Valor:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(115, 62);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 13);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Und:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(115, 17);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Descrição:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(247, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Qtd Inicial:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Código:";
+            // 
+            // PainelBotoes
+            // 
+            this.PainelBotoes.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.PainelBotoes.Controls.Add(this.btnSalvar);
+            this.PainelBotoes.Controls.Add(this.btnExcluir);
+            this.PainelBotoes.Controls.Add(this.btnVoltar);
+            this.PainelBotoes.Location = new System.Drawing.Point(542, 27);
+            this.PainelBotoes.Name = "PainelBotoes";
+            this.PainelBotoes.Size = new System.Drawing.Size(246, 27);
+            this.PainelBotoes.TabIndex = 5;
+            this.PainelBotoes.Visible = false;
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.btnSalvar.Location = new System.Drawing.Point(3, 3);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(75, 23);
+            this.btnSalvar.TabIndex = 0;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.ForeColor = System.Drawing.Color.Red;
+            this.btnExcluir.Location = new System.Drawing.Point(84, 3);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(75, 23);
+            this.btnExcluir.TabIndex = 1;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            // 
+            // btnVoltar
+            // 
+            this.btnVoltar.Location = new System.Drawing.Point(165, 3);
+            this.btnVoltar.Name = "btnVoltar";
+            this.btnVoltar.Size = new System.Drawing.Size(75, 23);
+            this.btnVoltar.TabIndex = 2;
+            this.btnVoltar.Text = "Voltar";
+            this.btnVoltar.UseVisualStyleBackColor = true;
+            this.btnVoltar.Click += new System.EventHandler(this.btnVoltar_Click_1);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(12, 104);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(757, 288);
+            this.dataGridView2.TabIndex = 10;
+            // 
+            // TelaPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.PainelBotoes);
+            this.Controls.Add(this.panelCadastroProduto);
+            this.Controls.Add(this.PainelCadastroUnidadeMedida);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "TelaPrincipal";
+            this.Text = "TelaPrincipal";
+            this.Load += new System.EventHandler(this.TelaPrincipal_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.PainelCadastroUnidadeMedida.ResumeLayout(false);
+            this.PainelCadastroUnidadeMedida.PerformLayout();
+            this.panelCadastroProduto.ResumeLayout(false);
+            this.panelCadastroProduto.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.txtQuantidade)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtvalor)).EndInit();
+            this.PainelBotoes.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem cadastrosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem unidadeDeMedidaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem produtosToolStripMenuItem;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblSigla;
+        private System.Windows.Forms.Label lblDescricao;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.TextBox txtSigla;
+        private System.Windows.Forms.TextBox txtDescricao;
+        private System.Windows.Forms.Panel PainelCadastroUnidadeMedida;
+        private System.Windows.Forms.Panel panelCadastroProduto;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown txtQuantidade;
+        private System.Windows.Forms.ComboBox comboUnd;
+        private System.Windows.Forms.NumericUpDown txtvalor;
+        private System.Windows.Forms.TextBox txtDescricaoProduto;
+        private System.Windows.Forms.TextBox txtCodigoProduto;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.FlowLayoutPanel PainelBotoes;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.Button btnVoltar;
+        private System.Windows.Forms.DataGridView dataGridView2;
+    }
+}
---
+++ Telas/TelaPrincipal.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GestorDeEstoque.Telas
+{
+    public partial class TelaPrincipal : Form
+    {
+        public TelaPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStripLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void unidadeDeMedidaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            PainelCadastroUnidadeMedida.Visible = true;
+            PainelBotoes.Visible = true;
+            panelCadastroProduto.Visible = false;
+        }
+
+        private void btnVoltar_Click(object sender, EventArgs e)
+        {
+            PainelCadastroUnidadeMedida.Visible = false;
+            PainelBotoes.Visible = false;
+            panelCadastroProduto.Visible = false;
+
+        }
+
+        private void PainelCadastroUnidadeMedida_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void TelaPrincipal_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void produtosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            panelCadastroProduto.Visible = true;
+            PainelBotoes.Visible = true;
+            PainelCadastroUnidadeMedida.Visible = false;
+        }
+
+        private void PainelBotoes_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btnVoltar_Click_1(object sender, EventArgs e)
+        {
+            PainelBotoes.Visible = false;
+            PainelCadastroUnidadeMedida.Visible = false;
+            panelCadastroProduto.Visible = false;
+        }
+    }
+}
